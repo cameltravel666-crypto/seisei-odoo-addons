098b5bc59b4617a02740e9411b48f2d07eb65bc4
Joshtt5 <jos.zhang@gmail.com>
2026-02-10 13:50:23 +0900

    Revert "feat(account-reports): add interactive financial reports module"
    This reverts commit 5462da7837b25757c9bff997fa6e9ad821b0fba0.

--- odoo_modules/seisei/seisei_account_reports/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import account_report
-from . import account_report_engine
-from . import account_report_actions
-from . import account_report_custom_handler
-from . import account_general_ledger
--- odoo_modules/seisei/seisei_account_reports/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import test_report_engine
-from . import test_general_ledger
